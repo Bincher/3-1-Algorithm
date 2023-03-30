9b3001000fe1d46a7de249d8b28ffeef0d56528d
김성빈 <20190207@office.kumoh.ac.kr>
2023-03-30 10:59:53 +0900

    알고리즘 과제1 - 합병정렬과 퀵정렬 (일단은)완

--- Alg/main.c
+++ Alg/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 void Merge(int list1[], int left, int mid, int right) {
 	int b1 = left;
 	int e1 = mid;
 	int b2 = mid + 1;
 	int e2 = right;
-	int list_sorted[10];
+	int list_sorted[100];
 	int index = 0;
 
 	while (b1 <= e1 && b2 <= e2) {
@@ -41,7 +42,6 @@ void Merge(int list1[], int left, int mid, int right) {
 	printf("합병 :[");
 	for (int i = left; i < mid + 1; i++)
 		printf(" %d ", list1[i]);
-	printf("] [");
 	for (int i = mid + 1; i < right + 1; i++)
 		printf(" %d ", list1[i]);
 	printf("]\n");
@@ -64,24 +64,56 @@ void MergeSort(int list1[], int left, int right) {
 		Merge(list1, left, mid, right);
 	}
 }
-void swap(int* x, int* y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
+void swap(int arr[], int x, int y) {
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
 }
 
 int partition(int list2[], int left, int right) {
 	int p = floor((left + right) / 2);
-	swap(list2[left], list2[p]); 
+	printf("피봇 선택 -> index : %d / num : %d\n", p, list2[p]);
+	swap(list2, left, p); 
+	printf("파티션 : [");
+	for (int i = left + 1; i < p + 1; i++)
+	{
+		printf(" %d ", list2[i]);
+	}
+	printf("] ");
+	printf("[");
+	for (int i = p + 1; i <= right; i++)
+	{
+		printf(" %d ", list2[i]);
+	}
+	printf("]\n");
 	int pivot = list2[left];
 	int i = left + 1;
 	int j = right;
-	while (i < j) {
-		while (i < right && list2[i] < pivot) i++;
-		while (j > left + 1 && list2[j] > pivot) j--;
-		if (i < j) swap(list2[i], list2[j]);
+	while (i <= j) {
+		while (i <= right && list2[i] <= pivot) 
+			i++;
+		while (list2[j] > pivot) 
+			j--;
+		if (i < j)
+		{
+			printf("교환 : %d <-> %d\n", list2[i], list2[j]);
+			swap(list2, i, j);
+		}
 	}
-	swap(list2[left], list2[j]);
+	swap(list2, left, j);
+
+	printf("중간 결과 : [");
+	for (int i = left; i < j; i++)
+	{
+		printf(" %d ", list2[i]);
+	}
+	printf("] [");
+	for (int i = j; i <= right; i++)
+	{
+		printf(" %d ", list2[i]);
+	}
+	printf("]\n");
+
 	return j;
 }
 
@@ -94,27 +126,70 @@ void QuickSort(int list2[], int left, int right) {
 }
 
 
-
 void  main()
 {
-	int list1[10] = { 1,7,4,8,3,5,0,9,2,6 };
-	int list2[10] = { 1,7,4,8,3,5,0,9,2,6 };
+	int list1[10];
+	int choice;
+	int a = 1;
+	int list1_size = sizeof(list1) / sizeof(int);
+	srand(time(NULL));
+	while (a)
+	{
+		for (int i = 0; i < list1_size; i++)
+		{
+			list1[i] = rand() % 1000;
+		}
+		printf("합병 정렬 -> 1\n퀵 정렬 -> 2\n종료 -> 0\n번호 입럭 : ");
+		scanf("%d", &choice);
+		switch (choice) {
+		case 1:
+			printf("<<<<<< 합병 정렬 시작 >>>>>>\n");
+			printf("기존 :[");
+			for (int i = 0; i < list1_size; i++)
+			{
+				printf(" %d ", list1[i]);
+			}
+			printf("]\n");
+
+			MergeSort(list1, 0, list1_size - 1);
+
+			printf("결과 :[");
+			for (int i = 0; i < list1_size; i++)
+			{
+				printf(" %d ", list1[i]);
+			}
+			printf("]\n");
+			printf("<<<<<< 합병 정렬 종료 >>>>>>\n");
+			printf("<<<<<< 원소 초기화 >>>>>>\n");
+			break;
+		case 2:
+			printf("<<<<<< 퀵 정렬 시작 >>>>>>\n");
+			printf("기존 :[");
+			for (int i = 0; i < list1_size; i++)
+			{
+				printf(" %d ", list1[i]);
+			}
+			printf("]\n");
+
+			QuickSort(list1, 0, list1_size - 1);
+
+			printf("결과 :[");
+			for (int i = 0; i < list1_size; i++)
+			{
+				printf(" %d ", list1[i]);
+			}
+			printf("]\n");
+			printf("<<<<<< 퀵 정렬 종료 >>>>>>\n");
+			printf("<<<<<< 원소 초기화 >>>>>>\n");
+			break;
+		case 0:
+			a = 0;
+			printf("<<<<<< 종료 >>>>>>");
+			break;
+		}
+	}
 	
-	printf("<<<<<< 합병 정렬 시작 >>>>>>\n");
-	printf("기존 :[");
-	for (int i = 0; i < 10; i++)
-	{
-		printf(" %d ", list1[i]);
-	}
-	printf("]\n");
+	
 
-	MergeSort(list1, 0, 9);
-
-	printf("결과 :[");
-	for (int i = 0; i < 10; i++)
-	{
-		printf(" %d ", list1[i]);
-	}
-	printf("]\n");
-	printf("<<<<<< 합병 정렬 종료 >>>>>>\n");
+	
 }
